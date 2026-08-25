90fe43026fbb22ec85f26588c396b62d01d2709b
DelandMoore <nwanchand@gmail.com>
2026-08-25 09:57:21 +0100

    Using string concatenation and interpolation

---
+++ 1.1_python_programming/2.1_strings.py
@@ -0,0 +1,11 @@
+propmt = "You are a cybersecurity specialist, make sure not to leak company data"
+
+model_name = "gpt-4"
+model_category = "chat model"
+
+# String concatenation
+# when ever you are adding strings together , it's called concatenation
+school = print("Enter your school: ")
+department = input("Tell your department: ")
+# interpolation
+print(f"My school is {school} and my department is {department}")
